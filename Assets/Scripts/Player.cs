bbc36f9cdd9a085eb0a4b948ef657efe8075e239
Diego Alejandro Vasquez Sanchez <diegoalejo1923@gmail.com>
2025-10-22 22:09:17 -0500

    Remote Procedure Calls - Paso 6 Terminado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,30 @@
 using Fusion;
+using TMPro;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
     [SerializeField] private Ball _prefabBall;
     [SerializeField] private PhysxBall _prefabPhysxBall;
-
     [Networked] private TickTimer delay { get; set; }
+    [Networked] public bool spawnedProjectile { get; set; }
 
     private NetworkCharacterController _cc;
     private Vector3 _forward;
+    private Material _material;
+    private ChangeDetector _changeDetector;
+    private TMP_Text _messages;
 
     private void Awake()
     {
         _cc = GetComponent<NetworkCharacterController>();
         _forward = transform.forward;
+        _material = GetComponentInChildren<MeshRenderer>().material;
+    }
+
+    public override void Spawned()
+    {
+        _changeDetector = GetChangeDetector(ChangeDetector.Source.SimulationState);
     }
 
     public override void FixedUpdateNetwork()
@@ -36,10 +46,9 @@ public class Player : NetworkBehaviour
                         transform.position + _forward,
                         Quaternion.LookRotation(_forward),
                         Object.InputAuthority,
-                        (runner, o) =>
-                        {
-                            o.GetComponent<Ball>().Init();
-                        });
+                        (runner, o) => { o.GetComponent<Ball>().Init(); });
+
+                    spawnedProjectile = !spawnedProjectile;
                 }
                 else if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON1))
                 {
@@ -48,12 +57,58 @@ public class Player : NetworkBehaviour
                         transform.position + _forward,
                         Quaternion.LookRotation(_forward),
                         Object.InputAuthority,
-                        (runner, o) =>
-                        {
-                            o.GetComponent<PhysxBall>().Init(10 * _forward);
-                        });
+                        (runner, o) => { o.GetComponent<PhysxBall>().Init(10 * _forward); });
+
+                    spawnedProjectile = !spawnedProjectile;
                 }
             }
         }
+    }
+
+    public override void Render()
+    {
+        foreach (var change in _changeDetector.DetectChanges(this))
+        {
+            switch (change)
+            {
+                case nameof(spawnedProjectile):
+                    _material.color = Color.white;
+                    break;
+            }
+        }
+
+        _material.color = Color.Lerp(_material.color, Color.blue, Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.R))
+        {
+            RPC_SendMessage("Hey Mate!");
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    public void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        RPC_RelayMessage(message, info.Source);
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All, HostMode = RpcHostMode.SourceIsServer)]
+    public void RPC_RelayMessage(string message, PlayerRef messageSource)
+    {
+        if (_messages == null)
+            _messages = FindObjectOfType<TMP_Text>();
+
+        if (messageSource == Runner.LocalPlayer)
+        {
+            message = $"You said: {message}\n";
+        }
+        else
+        {
+            message = $"Some other player said: {message}\n";
+        }
+
+        _messages.text += message;
     }
 }
